b093ab9fadd314de198e98b00a1837a9e026fb1d
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-02-23 11:43:47 +0600

    Added a new view page and route linked

---
+++ resources/views/players/players.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VFCA | Players</title>
+</head>
+<body>
+    <h1>Available Players</h1>
+    <ul>
+        <li>Cristiano Ronaldo</li>
+    </ul>
+</body>
+</html>
